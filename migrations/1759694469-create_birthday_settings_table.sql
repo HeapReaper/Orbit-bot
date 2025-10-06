4707dea35d3e04df2e350b5324b084737b139c00
HeapReaper <kelvin@aerobytes.nl>
2025-10-06 18:59:26 +0200

    Added module with migration

---
+++ migrations/1759694469-create_birthday_settings_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `birthday_settings` (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `guild_id` VARCHAR(22) NOT NULL UNIQUE,
+    `channel` VARCHAR(22) NOT NULL,
+    `message` TEXT NOT NULL,
+    `time` TIME NOT NULL,
+    `enabled` INT NOT NULL,
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
