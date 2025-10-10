35ed90816a2775858bc298b3558e3e111f5e959b
HeapReaper <kelvin@aerobytes.nl>
2025-10-10 13:54:33 +0200

    Working ticket system

--- modules/Tickets/commands.ts
+++ modules/Tickets/commands.ts
@@ -1,5 +1,34 @@
-import { SlashCommandBuilder } from "discord.js";
+import { SlashCommandBuilder } from 'discord.js';
 
 export const commands = [
-// @ts-ignore
+  new SlashCommandBuilder()
+    .setName('ticket')
+    .setDescription('Create tickets')
+    .addSubcommand(report =>
+      report
+        .setName('admins')
+        .setDescription('Open a ticket for the administration')
+        .addStringOption(option =>
+          option
+            .setName('reason')
+            .setDescription('The reason for the ticket')
+            .setRequired(true)
+        )
+    )
+    .addSubcommand(confidential =>
+      confidential
+        .setName('confidential')
+        .setDescription('Open a ticket that only the owner can see')
+        .addStringOption(option =>
+          option
+            .setName('reason')
+            .setDescription('The reason for the ticket')
+            .setRequired(true)
+        )
+    )
+    .addSubcommand(close =>
+      close
+        .setName('close')
+        .setDescription('Close a ticket')
+    )
 ].map(commands => commands.toJSON());
