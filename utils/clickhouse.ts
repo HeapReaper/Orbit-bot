09a769808860c47c0c5e4e3d966036aa4b1536cb
HeapReaper <kelvin@aerobytes.nl>
2025-10-14 17:31:17 +0200

    Disabled Clickhouse

--- utils/clickhouse.ts
+++ utils/clickhouse.ts
@@ -1,17 +1,17 @@
-import { createClient } from "@clickhouse/client";
-import * as process from "node:process";
-
-let clickhouseClient: any;
-
-try {
-  clickhouseClient = createClient({
-    username: process.env.CLICKHOUSE_USER,
-    password: process.env.CLICKHOUSE_PASS,
-    database: process.env.CLICKHOUSE_DB,
-    url: process.env.CLICKHOUSE_HOST,
-  });
-} catch (err) {
-  console.error("Failed to create ClickHouse client:", err);
-}
-
-export { clickhouseClient };
+// import { createClient } from "@clickhouse/client";
+// import * as process from "node:process";
+//
+// let clickhouseClient: any;
+//
+// try {
+//   clickhouseClient = createClient({
+//     username: process.env.CLICKHOUSE_USER,
+//     password: process.env.CLICKHOUSE_PASS,
+//     database: process.env.CLICKHOUSE_DB,
+//     url: process.env.CLICKHOUSE_HOST,
+//   });
+// } catch (err) {
+//   console.error("Failed to create ClickHouse client:", err);
+// }
+//
+// export { clickhouseClient };
