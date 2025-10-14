6a166dee24739e36c6e657cb4cdbc559b694a1f8
HeapReaper <kelvin@aerobytes.nl>
2025-10-14 19:06:47 +0200

    Updated clickhouse client

--- utils/clickhouse.ts
+++ utils/clickhouse.ts
@@ -8,7 +8,8 @@ try {
     username: process.env.CLICKHOUSE_USER,
     password: process.env.CLICKHOUSE_PASS,
     database: process.env.CLICKHOUSE_DB,
-    url: process.env.CLICKHOUSE_HOST,
+    host: process.env.CLICKHOUSE_HOST,
+    url: process.env.CLICKHOUSE_URL,
   });
 } catch (err) {
   console.error("Failed to create ClickHouse client:", err);
