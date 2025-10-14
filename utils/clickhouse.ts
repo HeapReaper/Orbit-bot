8aad700b92307db13e0cffb0a32312d098b7f27e
HeapReaper <kelvin@aerobytes.nl>
2025-10-14 16:25:58 +0200

    Added try catch for clickhouse

--- utils/clickhouse.ts
+++ utils/clickhouse.ts
@@ -1,9 +1,17 @@
-import {createClient} from "@clickhouse/client";
+import { createClient } from "@clickhouse/client";
 import * as process from "node:process";
 
-export const clickhouseClient = createClient({
-  username: process.env.CLICKHOUSE_USER,
-  password: process.env.CLICKHOUSE_PASS,
-  database: process.env.CLICKHOUSE_DB,
-  url: process.env.CLICKHOUSE_HOST,
-});
+let clickhouseClient;
+
+try {
+  clickhouseClient = createClient({
+    username: process.env.CLICKHOUSE_USER,
+    password: process.env.CLICKHOUSE_PASS,
+    database: process.env.CLICKHOUSE_DB,
+    url: process.env.CLICKHOUSE_HOST,
+  });
+} catch (err) {
+  console.error("Failed to create ClickHouse client:", err);
+}
+
+export { clickhouseClient };
