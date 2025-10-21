8dcd4fe6595259ca377f418a53cf15a268b916c4
HeapReaper <kelvin@aerobytes.nl>
2025-10-21 14:25:31 +0200

    Added leave message

--- modules/LeaveMessage/events.ts
+++ modules/LeaveMessage/events.ts
@@ -1,13 +1,79 @@
-import { Client, TextChannel } from "discord.js";
+import {
+  Client,
+  TextChannel,
+  Events as DiscordEvents,
+} from "discord.js";
+import Database from "@utils/database";
+import {GuildLogger} from "@utils/guildLog";
+import { PrismaClient } from "@prisma/client";
 
 let instance: Events | null = null;
 
 export default class Events {
   private readonly client: Client;
+  private prisma;
 
   constructor(client: Client) {
     this.client = client;
+    this.prisma = new PrismaClient();
+
     if (instance) return instance;
     instance = this;
+
+    this.client.on(DiscordEvents.GuildMemberRemove, async (member) => {
+      void this.event(member);
+    });
+  }
+
+  async event(member: any) {
+    const res = await this.prisma.leave_message_settings.findFirst({
+      where: {
+        guild_id: member.guild_id,
+        enabled: 1,
+      }
+    });
+
+    if (!res || !res.messages) return;
+
+    let channel: TextChannel | null = null;
+    try {
+      const fetchedChannel = await this.client.channels.fetch(res.channel as string);
+      if (fetchedChannel?.isTextBased()) {
+        channel = fetchedChannel as TextChannel;
+      }
+    } catch (err) {
+      GuildLogger.error(res.guild_id, "Welcome message channel could not be fetched. Please configure it again.");
+      return;
+    }
+
+    if (!channel) {
+      GuildLogger.error(res.guild_id, "Welcome message channel could not be found. Please configure it again.");
+      return;
+    }
+
+    let messages: string[] = [];
+    try {
+      messages = Array.isArray(res.messages)
+        ? res.messages
+        // @ts-ignore
+        : JSON.parse(res.messages);
+    } catch (err) {
+      GuildLogger.error(res.guild_id, "Could not parse welcome messages array.");
+      return;
+    }
+
+    if (!messages.length) return;
+
+    let messageToSend: string;
+    if (res.randomize) {
+      const randomIndex = Math.floor(Math.random() * messages.length);
+      messageToSend = messages[randomIndex];
+    } else {
+      messageToSend = messages[0];
+    }
+
+    const updatedMessage = messageToSend.replace(/{user}/g, `<@${member.user.id}>`);
+
+    await channel.send({ content: updatedMessage });
   }
 }
