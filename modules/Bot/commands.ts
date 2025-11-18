50bd18884fcc75556c459acd63888b69f743dae6
HeapReaper <kelvin@aerobytes.nl>
2025-11-18 10:57:12 +0100

    Added status command

--- modules/Bot/commands.ts
+++ modules/Bot/commands.ts
@@ -1,5 +1,7 @@
 import { SlashCommandBuilder } from "discord.js";
 
 export const commands = [
-
+  new SlashCommandBuilder()
+    .setName("status")
+    .setDescription("See the status of this bot and the dashboard")
 ].map(commands => commands.toJSON());
